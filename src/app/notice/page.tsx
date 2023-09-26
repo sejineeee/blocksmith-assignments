e387b42a9996db4a94a2adc8055c6f26fae88349
sejineeee <sejinee0918@gmail.com>
2023-09-26 23:55:46 +0900

    feat: route notice page 구현
    axios 이용하여 api 호출
    useState 이용해서 api 호출 데이터 관리
    삼항연산자 이용하여 table 또는 메시지 보여주도록 구현
    
    현재 handleChange 함수는 미구현 상태로 구현할 예정임

---
+++ src/app/notice/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+
+import Input from "../components/Input";
+import Table from "../components/Table";
+import EmptyMessage from "../components/EmptyMessage";
+
+import "../styles/noticePage.scss";
+
+const Notice = (): JSX.Element => {
+  const [list, setList] = useState([]);
+
+  const getNoticeList = async () => {
+    try {
+      const response = await axios.get("http://localhost:9999/notice");
+
+      setList(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getNoticeList();
+  }, []);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {};
+
+  return (
+    <div className="notice-container">
+      <div className="notice-header">
+        <h2>공지사항</h2>
+        <Input
+          type="text"
+          name="notice-search"
+          placeholder="검색어"
+          disabled={false}
+          onChange={handleChange}
+        />
+      </div>
+      {list.length ? (
+        <Table noticeList={list} />
+      ) : (
+        <EmptyMessage message="공지사항이 없습니다." />
+      )}
+    </div>
+  );
+};
+
+export default Notice;
