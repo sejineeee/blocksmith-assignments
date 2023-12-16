4509f56b7d15b4be95f29e263e7d52eef17c74c3
sejineeee <sejinee0918@gmail.com>
2023-12-16 16:42:12 +0900

    feature: react-quill editor component

---
+++ src/app/components/Editor.tsx
@@ -0,0 +1,39 @@
+import ReactQuill from 'react-quill';
+
+import 'react-quill/dist/quill.snow.css';
+import '../styles/editor.scss';
+
+const tools = [
+  [{ size: ['small', 'false', 'large', 'huge'] }],
+  [{ font: ['sans-serif', 'serif', 'monospace'] }],
+  ['bold', 'italic', 'underline'],
+  [{ color: [] }, { background: [] }],
+  [{ list: 'ordered' }, { list: 'bullet' }],
+  [{ align: [] }],
+  ['link', 'image', 'code-block'],
+  [{ direction: 'rtl' }],
+];
+
+const modules = {
+  toolbar: tools,
+};
+
+interface EditorProps {
+  content: string;
+  setContent: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const Editor = ({ content, setContent }: EditorProps): JSX.Element => {
+  return (
+    <div id="editor">
+      <ReactQuill
+        modules={modules}
+        theme="snow"
+        value={content}
+        onChange={setContent}
+      />
+    </div>
+  );
+};
+
+export default Editor;
