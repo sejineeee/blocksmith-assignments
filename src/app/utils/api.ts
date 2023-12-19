f663319ce6348d887d54b83bdf4623ede3bcaef4
sejineeee <sejinee0918@gmail.com>
2023-12-19 15:21:24 +0900

    fix: api 호출 함수 export

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const getNoticeList = async () => {
+export const getNoticeList = async () => {
   try {
     const response = await axios.get('http://localhost:9999/notice');
     return response.data;
