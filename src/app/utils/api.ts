7ec102b6ba0fdd2dd1a5f9a8a6c83819023ed7bd
sejineeee <sejinee0918@gmail.com>
2023-12-19 15:43:24 +0900

    feature: notice create api 호출 함수 구현

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -1,9 +1,31 @@
+import { Notice } from '@/types/notice';
 import axios from 'axios';
 
 export const getNoticeList = async () => {
   try {
     const response = await axios.get('http://localhost:9999/notice');
     return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const createNotice = async ({ id, title, date, content }: Notice) => {
+  try {
+    const response = await axios.post(
+      `http://localhost:9999/notice/`,
+      {
+        id,
+        title,
+        date,
+        content,
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
   } catch (error) {
     console.error(error);
   }
