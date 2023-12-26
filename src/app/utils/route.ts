3a8d3a92fa22df57ed52dc5b96ca9fc056474c1f
sejineeee <sejinee0918@gmail.com>
2023-12-26 23:34:01 +0900

    feature: get, post route handler 구현

---
+++ src/app/utils/route.ts
@@ -0,0 +1,30 @@
+import { client } from '@/libs/client';
+import { NextResponse } from 'next/server';
+
+export const GET = async () => {
+  try {
+    const posts = await client.post.findMany();
+
+    return NextResponse.json(posts);
+  } catch (error) {
+    return NextResponse.json({ message: 'GET ERROR', error }, { status: 500 });
+  }
+};
+
+export const POST = async (request: Request) => {
+  try {
+    const body = await request.json();
+    const { title, content } = body;
+
+    const newPost = await client.post.create({
+      data: {
+        title,
+        content,
+      },
+    });
+
+    return NextResponse.json(newPost);
+  } catch (error) {
+    return NextResponse.json({ message: 'POST ERROR', error }, { status: 500 });
+  }
+};
