fc4b57d51006a44b58166637fb341441336aaf86
sejineeee <sejinee0918@gmail.com>
2023-12-28 10:07:33 +0900

    feature: 상세 페이지에서 사용하는 api 구현

---
+++ src/app/api/notice/[id]/route.ts
@@ -0,0 +1,72 @@
+import { client } from '@/libs/client';
+import { NextResponse } from 'next/server';
+
+export const GET = async ({ params }: { params: { id: string } }) => {
+  try {
+    const { id } = params;
+    const post = await client.post.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!post) {
+      return NextResponse.json({ message: 'Post not found' }, { status: 404 });
+    }
+    return NextResponse.json(post);
+  } catch (error) {
+    return NextResponse.json({ message: 'GET ERROR', error }, { status: 500 });
+  }
+};
+
+export const PATCH = async (
+  request: Request,
+  { params }: { params: { id: string } }
+) => {
+  try {
+    const { id } = params;
+
+    const body = await request.json();
+    const { title, content } = body;
+
+    const updatePost = await client.post.update({
+      where: {
+        id,
+      },
+      data: {
+        title,
+        content,
+      },
+    });
+
+    if (!updatePost) {
+      return NextResponse.json({ message: 'Post not found' }, { status: 404 });
+    }
+
+    return NextResponse.json(updatePost);
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'UPDATE ERROR', error },
+      { status: 500 }
+    );
+  }
+};
+
+export const DELETE = async ({ params }: { params: { id: string } }) => {
+  try {
+    const { id } = params;
+
+    await client.post.delete({
+      where: {
+        id,
+      },
+    });
+
+    return NextResponse.json('Post has been deleted');
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'DELETE ERROR', error },
+      { status: 500 }
+    );
+  }
+};
