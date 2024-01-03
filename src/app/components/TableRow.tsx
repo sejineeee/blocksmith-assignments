4bc77d637f61b821558bd46af83b293755abebca
sejineeee <sejinee0918@gmail.com>
2024-01-03 23:25:37 +0900

    modify: 날짜 부분 값 수정
    id 값에서 customDate 값 렌더링으로 수정
    customDate LocaleString으로 변환
    code style formatter

--- src/app/components/TableRow.tsx
+++ src/app/components/TableRow.tsx
@@ -1,15 +1,20 @@
-import { Notice } from "@/types/notice";
+import { Notice } from '@/types/notice';
 
 interface TableRowProps {
   notice: Notice;
   onClick: () => void;
 }
 
-const TableRow = ({ notice: { title, id }, onClick}: TableRowProps): JSX.Element => {
+const TableRow = ({
+  notice: { title, customDate },
+  onClick,
+}: TableRowProps): JSX.Element => {
+  const parsedCustomDate = new Date(customDate).toLocaleString('ko-KR');
+
   return (
     <tr className="table-row" onClick={onClick}>
       <td className="title">{title}</td>
-      <td className="created-at">{id}</td>
+      <td className="created-at">{parsedCustomDate}</td>
     </tr>
   );
 };
