e9e05d1499a7fae39c84affa77bc98ed68c22669
sejineeee <sejinee0918@gmail.com>
2024-01-17 13:52:50 +0900

    feature:  게시글 생성의 상대적인 시간 렌더링 구현

--- src/app/components/TableRow.tsx
+++ src/app/components/TableRow.tsx
@@ -9,12 +9,36 @@ const TableRow = ({
   notice: { title, customDate },
   onClick,
 }: TableRowProps): JSX.Element => {
-  const parsedCustomDate = new Date(customDate).toLocaleString('ko-KR');
+  const now = new Date();
+  const created = new Date(customDate);
+
+  const timeDifference = Math.floor((now.getTime() - created.getTime()) / 1000);
+
+  const getFormatTimeAgo = (timeDifference: number): string => {
+    if (timeDifference < 60) {
+      return '방금 전';
+    } else if (timeDifference < 3600) {
+      const minutesDifference = Math.floor(timeDifference / 60);
+
+      return `${minutesDifference}분 전`;
+    } else if (timeDifference < 86400) {
+      const hoursDifference = Math.floor(timeDifference / 3600);
+      console.log('timeDifference', timeDifference, 'hours', hoursDifference);
+
+      return `${hoursDifference}시간 전`;
+    } else {
+      const parsedCreatedDate = created.toLocaleString('ko-KR');
+
+      return parsedCreatedDate;
+    }
+  };
+
+  const formatCreatedDate = getFormatTimeAgo(timeDifference);
 
   return (
     <tr className="table-row" onClick={onClick}>
       <td className="title">{title}</td>
-      <td className="created-at">{parsedCustomDate}</td>
+      <td className="created-at">{formatCreatedDate}</td>
     </tr>
   );
 };
