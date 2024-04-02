b8b31597c1f8711d1ffc09efcbfcc7a16e089bd2
sejineeee <sejinee0918@gmail.com>
2024-04-02 16:11:51 +0900

    style: todo 주석 제거

--- src/app/components/Pagination.tsx
+++ src/app/components/Pagination.tsx
@@ -17,12 +17,6 @@ const Pagination = ({
     (_, index) => index + 1
   );
 
-  // TODO
-  // 1. page 번호에 맞게 게시글을 잘 나오지만, 페이지네이션 번호가 나오질 않음
-  // 2. 게시글의 총 갯수를 가져오는 방법을 생각해서 총 개수를 가지고 페이지 번호
-  // 만들 생각을 해야 된다.
-  // 3. 화살표 버튼 처리에 대해서도 생각해야 된다
-
   return (
     <div className="pagination">
       <button className="button-previous-group">
